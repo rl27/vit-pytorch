e7fec03aa95e067dbeb2e093f010516960549ec5
rl27 <rl27@princeton.edu>
2022-03-31 18:51:55 -0400

    Fix pos embedding dim

--- vit_pytorch/vit.py
+++ vit_pytorch/vit.py
@@ -148,7 +148,7 @@ class ViT(nn.Module):
         )
         '''
 
-        self.pos_embedding = nn.Parameter(torch.randn(1, num_patches + 1, dim))
+        self.pos_embedding = nn.Parameter(torch.randn(1, num_patches + 1, 200))
         self.cls_token = nn.Parameter(torch.randn(1, 1, 200))
         self.dropout = nn.Dropout(emb_dropout)
 
